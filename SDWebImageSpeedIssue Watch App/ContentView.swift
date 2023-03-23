a1e749f8ec2ee9444d6a88871966cbcb5bf990c1
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2023-03-23 17:43:17 +0800

    Initial Commit

---
+++ SDWebImageSpeedIssue Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SDWebImageSpeedIssue Watch App
+//
+//  Created by Vincent Neo on 23/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SDWebImageSpeedIssue Watch App/SDWebImageSpeedIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDWebImageSpeedIssueApp.swift
+//  SDWebImageSpeedIssue Watch App
+//
+//  Created by Vincent Neo on 23/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SDWebImageSpeedIssue_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
